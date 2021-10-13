b4a15b5502438275cb105894b80d6917bb8393a3
Vinicius Cavalcanti <tux.cavalcanti@gmail.com>
2021-10-12 22:12:21 -0300

    Update crumb.sh

--- crumb.sh
+++ crumb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-crumb=$(curl -u "$JENKINS_USER:$JENKINS_PASS" -s 'http://http://44.194.143.68:8080//crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-curl -u "$JENKINS_USER:$JENKINS_PASS" -H "$crumb" -X POST http://http://44.194.143.68:8080//job/ENV/build?delay=0sec
+crumb=$(curl -u "$JENKINS_USER:$JENKINS_PASS" -s "http://$JENKINS_URL//crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)")
+curl -u "jenkins:1234" -H "$crumb" -X POST http://$JENKINS_URL//job/ENV/build?delay=0sec
